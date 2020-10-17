2c10303a7c23c1200d7d4be2b78b42790217c420
Eralp Karaduman <eralp@eralpkaraduman.com>
2020-10-17 03:02:08 +0300

    Created workspace setup

---
+++ HelloWorld/main.lua
@@ -0,0 +1,4 @@
+if pcall(require, "lldebugger") then require("lldebugger").start() end
+if pcall(require, "mobdebug") then require("mobdebug").start() end
+
+function love.draw() love.graphics.print('Hello World!', 400, 300) end
