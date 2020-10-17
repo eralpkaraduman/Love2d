dbbde33de22e01e8836050011c234d5f8992aee7
Eralp Karaduman <eralp@eralpkaraduman.com>
2020-10-17 03:47:39 +0300

    Added cake

---
+++ Callbacks/main.lua
@@ -0,0 +1,22 @@
+if pcall(require, "lldebugger") then require("lldebugger").start() end
+if pcall(require, "mobdebug") then require("mobdebug").start() end
+
+local image, width, height
+
+function love.load()
+    image = love.graphics.newImage("cake.png")
+    local f = love.graphics.newFont(54)
+    love.graphics.setFont(f)
+    love.graphics.setBackgroundColor(255, 255, 255)
+end
+
+function love.draw()
+
+    love.graphics.setColor(255, 255, 255, 255)
+    love.graphics.draw(image, 10, 10)
+
+    love.graphics.setColor(0, 0, 0, 255)
+    love.graphics.print('Cake!', 400, 300)
+end
+
+function love.quit() print("Thanks for playing! Come back soon!") end
