875dc69038234556945da218aabf086c99604fe3
Eralp Karaduman <eralp@eralpkaraduman.com>
2020-10-19 01:08:45 +0300

    Formatted code

--- HelloWorld/main.lua
+++ HelloWorld/main.lua
@@ -1,4 +1,10 @@
-if pcall(require, "lldebugger") then require("lldebugger").start() end
-if pcall(require, "mobdebug") then require("mobdebug").start() end
+if pcall(require, "lldebugger") then
+  require("lldebugger").start()
+end
+if pcall(require, "mobdebug") then
+  require("mobdebug").start()
+end
 
-function love.draw() love.graphics.print('Hello World!', 400, 300) end
+function love.draw()
+  love.graphics.print('Hello World!', 400, 300)
+end
